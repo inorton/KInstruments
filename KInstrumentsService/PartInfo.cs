5382952241e24ddad2c3318f649ecbb3bb8f0692
Ian Norton <inorton@gmail.com>
2014-01-08 15:27:28 +0000

    add part info

---
+++ KInstrumentsService/PartInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace KInstrumentsService
+{
+    [DataContract]
+    public class FuelInfo {
+
+        [DataMember]
+        public string FuelType { get; set; }
+
+        [DataMember]
+        public int Capacity { get; set; }
+
+        [DataMember]
+        public int Contents { get; set; }
+    }
+
+    [DataContract]
+    public class EngineInfo
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string[] FuelTypes { get; set; }
+    }
+
+    [DataContract]
+    public class PartInfo
+    {
+        [DataMember]
+        public string Id { get; set; }
+
+        public int Mass { get; set; }
+
+        [DataMember]
+        public FuelInfo[] Fuel { get; set; }
+
+        [DataMember]
+        public EngineInfo Engine { get; set; }
+
+        [DataMember]
+        public string[] Connections { get; set; }
+    }
+}
