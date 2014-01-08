0b7a60c639067e9e61cd1f250e6f8f6526585d88
Ian Norton <inorton@gmail.com>
2014-01-08 22:55:02 +0000

    objects!

--- KInstrumentsService/www/static/js/rpc.js
+++ KInstrumentsService/www/static/js/rpc.js
@@ -1,5 +1,50 @@
-﻿function getRPC() {
+﻿function KiClient() {
+    this.rpc = getJsonRPCProxy();
+    this.lastdata_msec = 0;
+    this.lastdata = null;
+    this.lastdata_max_msec = 100;
+}
+
+KiClient.prototype.SetThrottle = function (percent) {
+    if (percent > 1) {
+        percent = 1;
+    }
+    if (percent < 0) {
+        percent = 0;
+    }
+    this.rpc.SetThrottle({ Set: percent });
+};
+
+KiClient.prototype.SetTrim = function (trim) {
+    this.rpc.SetTrim({ Set: trim });
+};
+
+KiClient.prototype.ToggleGear = function () {
+    this.rpc.ToggleGear();
+};
+
+KiClient.prototype.ToggleStage = function () {
+    this.rpc.ToggleStage();
+};
+
+KiClient.prototype.PollData = function () {
+    var nowms = Date.now();
+    if (this.lastdata == null || (nowms - this.lastdata_msec) > this.lastdata_max_msec) {
+        this.lastdata_msec = nowms;
+        this.lastdata = this.rpc.PollData();
+    }
+    return this.lastdata;
+};
+
+KiClient.instance = new KiClient();
+
+function getRPC() {
+    return KiClient.instance;
+}
+
+function getJsonRPCProxy()
+{
     var rpc = new JSONRPCProxy('/json/',
-        ['PollData', 'Throttle', 'ToggleGear']);
+        ['PollData', 'SetTrim', 'SetThrottle', 'ToggleGear', 'ToggleStage']);
     return rpc;
 }
